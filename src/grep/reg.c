61744225c4396c429ad9700561238639889436e3
shushanik-petrosyan <shushanikpetrosyan@mail.ru>
2023-11-06 19:31:05 +0300

    добавление считывания строк с помощью динамической памяти

--- src/grep/reg.c
+++ src/grep/reg.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <regex.h>
 
 int main() {
+    char *pattern = NULL;
+    char *text = NULL;
+    size_t pattern_size = 0;
+    size_t text_size = 0;
 
-    
-    char pattern[] = "ab";
-    char text[] = "abcabcd a b ababcdef bac";
+    printf("Введите регулярное выражение: ");
+    getline(&pattern, &pattern_size, stdin);
+
+    printf("Введите текст: ");
+    getline(&text, &text_size, stdin);
+
+    // убираем символ переноса строки в конце строк
+    pattern[strcspn(pattern, "\n")] = '\0';
+    text[strcspn(text, "\n")] = '\0';
 
     regex_t regex;
     int reti;
@@ -28,8 +40,10 @@ int main() {
         return 1;
     }
 
-    // освобождаем память, занятую регулярным выражением
+    // освобождаем память, занятую регулярным выражением и введенными строками
     regfree(&regex);
+    free(pattern);
+    free(text);
 
     return 0;
 }
