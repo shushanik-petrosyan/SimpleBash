9fc32deb4a7a54e22c1a34d80f63a659413499d3
Шушаник Петросян <149955166+shushanik-petrosyan@users.noreply.github.com>
2023-11-19 19:09:20 +0300

    header

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define _GNU_SOURCE
+#define MEMORY_SIZE 64
 
 typedef struct options {
   int e;
@@ -22,8 +23,11 @@ typedef struct options {
   int o;
   char **patterns;
   int pattern_count;
+  int pattern_capacity;
+
   char **file_patterns;
   int file_pattern_count;
+  int file_pattern_capacity;
 } options;
 
 const char *short_options = "e:ivclnf:hso";
@@ -31,14 +35,13 @@ struct option long_options[] = {{0, 0, 0, 0}};
 
 bool parser(int argc, char **argv, struct options *options);
 void arg_number(int argc, char **argv, struct options *options);
-void file_check(char **argv, int i, struct options *options,
-                        int file_count);
+void file_check(char **argv, int i, struct options *options, int file_count);
 int basic_grep(char **argv, FILE *filename, int i, struct options *options,
                int file_count);
 void flag_h(char **argv, int i, struct options *options, int file_count,
             int *print_file_count);
 int reg_compilation(regex_t *regex, char *text, struct options *options,
-                            char **argv);
+                    char **argv);
 void flag_n(int *file_line_counter, struct options *options, int reti);
 void print_error();
 void flag_f(struct options *options, char **argv);
@@ -46,6 +49,10 @@ void flag_c(char **argv, int i, struct options *options, int file_count,
             int founded_pattern_count, int print_file_count);
 void flag_o(int reti, regmatch_t *matches, regex_t regex, char *text);
 void add_line_break(char *text);
-void memory_free (struct options *options);
+
+void memory_free(struct options *options);
+void add_pattern(char *pattern, options *options);
+void add_pattern_file(char *pattern, options *options);
+void memory_create(struct options *options);
 
 #endif
